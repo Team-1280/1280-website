d5209b7cfa477e480c89532b80dd1d8835482777
Nandini Ghata <152566146+NANO13579@users.noreply.github.com>
2025-06-21 08:19:34 -0700

    navbar - which doesn't work

--- website/src/App.js
+++ website/src/App.js
@@ -1,24 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from '../pages/Home';
-import About from '../pages/About';
-import Outreach from '../pages/Outreach';
-import Resources from '../pages/Resources';
+import Navbar from './components/Navbar.js';
 
 function App(){
-  return (
-    <Router>
-      <Navbar />
-      <div>
-        <Routes>
-          <Route path = "/" element={<Home />} />
-          <Route path = "/" element={<About />} />
-          <Route path = "/" element={<Outreach />} />
-          <Route path = "/" element={<Resources />} />
-        </Routes>
-      </div>
-    </Router>
-  )
-}
+    return (
+      <Router>
+        <Navbar />
+      </Router>
+    )
+};
 
 export default App()
