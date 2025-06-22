1dc25c176b0ee7ab9207091d1f0855a87db0523a
Nandini Ghata <152566146+NANO13579@users.noreply.github.com>
2025-06-21 22:11:12 -0700

    updated code using previous failed lack of build tool version

---
+++ app/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+
+const Navbar = () => {
+    <nav className="navbar">
+        <div className="arrangement">
+            <div className='links'>
+                <Link to='/'>Home</Link>
+                <Link to='/About'>About</Link>
+                <Link to='/Outreach'>Outreach</Link>
+                <Link to='/Resources'>Resources</Link>
+            </div>
+        </div>
+    </nav>
+}
+export default Navbar;
