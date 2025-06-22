725534ee105cead8258b60e61079563ed6b63f1b
Nandini Ghata <152566146+NANO13579@users.noreply.github.com>
2025-06-22 15:03:12 -0700

    functional navbar

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -2,16 +2,17 @@ import './Navbar.css';
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    <nav className="navbar">
-        <div>TESTING NAVBAR</div>
-        <div className="arrangement">
-            <div className='links'>
-                <Link to='/'>Home</Link>
-                <Link to='/About'>About</Link>
-                <Link to='/Outreach'>Outreach</Link>
-                <Link to='/Resources'>Resources</Link>
+    return (
+        <nav className="navbar">
+            <div className="arrangement">
+                <div className='links'>
+                    <Link to='/'>Home</Link>
+                    <Link to='/About'>About</Link>
+                    <Link to='/Outreach'>Outreach</Link>
+                    <Link to='/Resources'>Resources</Link>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
+    )
 }
 export default Navbar;
