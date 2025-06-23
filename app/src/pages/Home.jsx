6f6f58b1c65d9de74ff49171be1168c39d42bf59
Nandini Ghata <152566146+NANO13579@users.noreply.github.com>
2025-06-22 21:12:19 -0700

    updated mission statement css

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import './Home.css'
 
 const Home = () => (
-    <section className="hero-image">
-        <div className='hero-text'>
-            testingtesting
-        </div>
-    </section>
+    <> 
+        <section className="hero-image">
+            <div className='hero-text'>
+                1280
+                Ragin' C Biscuits
+            </div>
+        </section>
+        <section className="mission-background">
+            <div className='mission-heading'>
+                Our Mission
+            </div>
+            <div className='mission-text'>
+                blah blah mission stuff here
+            </div>
+        </section>
+    </>   
 )
 
 export default Home;
