7d14967c82db5e9466ebe32cbeba3be97e176d1c
Nandini Ghata <152566146+NANO13579@users.noreply.github.com>
2025-06-22 23:57:05 -0700

    mission statement formatted and action calls added

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -15,7 +15,23 @@ const Home = () => (
                 Our Mission
             </div>
             <div className='mission-text'>
-                blah blah mission stuff here
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        </section>
+        <section>
+            <div className='action-calls'>
+                <button className='action-button'>
+                    Donate to the Team
+                </button>
+                <button className='action-button'>
+                    Learn About Us
+                </button>
+                <button className='action-button'>
+                    Join the Team
+                </button>
             </div>
         </section>
     </>   
