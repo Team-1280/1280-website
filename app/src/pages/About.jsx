a237be75955d8b163ca305a9a3d35fbf1b6f0af2
Nandini Ghata <152566146+NANO13579@users.noreply.github.com>
2025-06-23 23:25:12 -0700

    about page banner and history formatted/updated

--- app/src/pages/About.jsx
+++ app/src/pages/About.jsx
@@ -1,7 +1,40 @@
+import './About.css'
+import TeamImage from '/team.png'
+
 const About = () => (
-    <div>
-        <h1>testing ABOUT</h1>
-    </div>
+    <>
+        <section className="banner-background">
+            <div className='banner-opacity'>
+                <div className='banner-heading'>
+                    Who We Are
+                </div>
+                <div className='banner-text'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </div>
+            </div>
+        </section>
+        <section>
+            <div className='split-container'>
+                <div className='half image-half'>
+                    <img src={TeamImage} alt="Team 1280" />
+                </div>
+                <div className='half text-half'>
+                    <div className='banner-heading'>
+                    Our History
+                    </div>
+                    <div className='banner-text'>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
 )
 
 export default About;
