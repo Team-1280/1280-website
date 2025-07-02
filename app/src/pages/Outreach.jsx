d3629d40362408b0faa24e6a6578612522277ab2
Nandini Ghata <152566146+nano13579@users.noreply.github.com>
2025-07-01 23:29:43 -0700

    update outreach - doesnt generate sad

--- app/src/pages/Outreach.jsx
+++ app/src/pages/Outreach.jsx
@@ -1,7 +1,19 @@
+import './styles/Outreach.css'
+
 const Outreach = () => {
-    <div>
-        <h1>testing OUTREACH</h1>
-    </div>
+    <>
+        <section>
+            <div className='outreach-page-top-background'>
+                <div className='outreach-page-heading'>
+                    Outreach
+                </div>
+                <div className='outreach-page-subheading'>
+                    At Team 1280 – the Ragin’ C’ Biscuits – we believe robotics is a platform for education, inspiration, and community impact. 
+                    Through our outreach efforts, we work to expand access to STEM and mentor the next generation of innovators.
+                </div>
+            </div>
+        </section>
+    </>
 }
 
 export default Outreach;
