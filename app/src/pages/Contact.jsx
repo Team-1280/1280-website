9d39d481b217700cabe17edad1ce241b475b8017
Nandini Ghata <152566146+nano13579@users.noreply.github.com>
2025-07-16 22:05:17 -0700

    finished about

--- app/src/pages/Contact.jsx
+++ app/src/pages/Contact.jsx
@@ -1,7 +1,45 @@
+import './styles/Contact.css'
+
 const Contact = () => (
-    <div>
-        <h1>testing Contact</h1>
-    </div>
+    <>
+    <section>
+        <div className='contact-page-heading'>
+            Contact Team 1280 – Ragin' C’ Biscuits
+        </div>
+        <div className='contact-page-subheading'>
+            Have a question about our team, sponsorship opportunities, outreach events, or joining as a member or mentor? We’d love to hear from you.
+        </div>
+    </section>
+    <section>
+        <div className='gen-inq-heading'>
+            General Inquiries
+        </div>
+        <div className='gen-inq-subheading'>
+            <div className='contact-newline'>
+            Email: sanramonvalleyrobotics@gmail.com
+            </div>
+            <div className='contact-newline'>
+            Mailing Address:
+            San Ramon Valley High School
+            501 Danville Blvd
+            Danville, CA 94526
+            </div>
+            <div className='contact-newline'>
+            Team Location: Danville, California
+            </div>
+        </div>
+        <div className='contact-interested-heading'>
+            Interested in Joining or Mentoring?
+        </div>
+        <div className='contact-interested-subheading'>
+            We’re always excited to welcome new students and mentors!
+            Visit our Join Us page or email us to learn more about:
+            How to join as a student
+            Mentorship roles (technical and non-technical)
+            Team events and training schedule
+        </div>
+    </section>
+    </>
 )
 
 export default Contact;
