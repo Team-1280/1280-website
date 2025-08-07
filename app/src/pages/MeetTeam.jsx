2aedf3533266507a8ebd056f6e8a75eee296db6b
Nandini Ghata <152566146+nano13579@users.noreply.github.com>
2025-08-07 00:15:39 -0700

    update outreach

--- app/src/pages/MeetTeam.jsx
+++ app/src/pages/MeetTeam.jsx
@@ -3,6 +3,18 @@ import './styles/MeetTeam.css'
 const MeetTeam = () => (
     <>
         <section>
+            <div className='bg-white relative min-h-screen w-screen px-7 py-5'>
+                <div className='relative bg-[#52565e] justify-center grid items-center w-300 h-60'>
+                    <div className='text-white text-3xl text-left font-bold px-10'>
+                        Outreach
+                    </div>
+                    <div className='text-white text-base text-left px-10 py-0'>
+                        As a dedicated FIRST Robotics Competition (FRC) team, we strive not only for excellence in competition but also to serve as a conduit for STEM education and the FIRST mission within our community. Over the years, we've actively hosted and participated in numerous local events aimed at inspiring and educating curious minds. Recently, our efforts have expanded to include the establishment of additional FIRST teams within our school district. As we continue to evolve and excel in FRC, we remain committed to fostering similar growth and enthusiasm for STEM in our community.
+                    </div>
+                </div>
+            </div>
+        </section> 
+        <section>
             <div className='page-background'>
                 <div className='top-header'>
                     Student Leadership
