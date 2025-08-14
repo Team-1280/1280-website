5ceee883be8d749eb502030401be5766b3a574a5
Nandini Ghata <152566146+nano13579@users.noreply.github.com>
2025-08-13 22:50:16 -0700

    update join with gray bg sections

--- app/src/pages/Join.jsx
+++ app/src/pages/Join.jsx
@@ -3,78 +3,99 @@ import './styles/Join.css'
 const Join = () => (
     <>
         <section>
-            <div className='text-left bg-white relative h-[110vh] w-screen'>
+            <div className='text-left bg-white relative h-[142vh] w-screen pl-12'>
                 <img src='/join1.png' className='absolute h-[30vh] top-[10%] left-[80%] -translate-x-1/2' alt="Join 1"></img>
                 <img src='/join2.png' className='absolute h-[30vh] top-[40%] left-[80%] -translate-x-1/2' alt="Join 2"></img>
                 <img src='/join3.png' className='absolute h-[30vh] top-[70%] left-[80%] -translate-x-1/2' alt="Join 3"></img>
                 <div className='px-10 py-10 text-black text-5xl font-bold'>How to Join Team 1280</div>
-                <div className='px-10 text-black text-2xl font-bold'>For Students:</div>
-                <div className='p-2'></div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>Interested in joining Team 1280? We welcome all students entering grades 9–12—no prior experience is required! Our team values curiosity, collaboration, and commitment, and we're excited to help new members grow in both technical and non-technical roles.</div>
-                <div className='p-2'></div>
-                <div className='px-10 text-black text-xl font-bold'>Application Process</div>
-                <div className='p-2'></div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>
-                    <ul className='list-disc pl-6'>
-                        <li>Application Period: Opens in August and closes in early December.</li>
-                        <li>Students who apply after the deadline or do not initially make the team will be placed on a waitlist.</li>
-                        <li>Have questions? Reach out to us at sanramonvalleyrobotics@gmail.com</li>
-                    </ul>
+                <div className='bg-[#717070] w-15/24 py-4 h-[137vh]'>
+                    <div className='px-10 text-[#fcc307] text-2xl font-bold'>For Students:</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>Interested in joining Team 1280? We welcome all students entering grades 9–12—no prior experience is required! Our team values curiosity, collaboration, and commitment, and we're excited to help new members grow in both technical and non-technical roles.</div>
+                    <div className='p-2'></div>
+                    <div className='px-10 text-[#fcc307] text-xl font-bold'>Application Process</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>
+                        <ul className='list-disc pl-6'>
+                            <li>Application Period: Opens in August and closes in early December.</li>
+                            <li>Students who apply after the deadline or do not initially make the team will be placed on a waitlist.</li>
+                            <li>Have questions? Reach out to us at sanramonvalleyrobotics@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div className='p-2'></div>
+                    <div className='px-10 text-[#fcc307] text-xl font-bold'>Application Links for 2025/2026 Season</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>
+                        <ul className='list-disc pl-6'>
+                            <li className='underline'>Team Membership Application Form</li>
+                            <li className='underline'>Liability Waiver</li>
+                            <ul className='list-disc pl-6'>
+                                <li>Submit in person to Team Co-Captain Rohan Gawde.</li>
+                            </ul>
+                            <li className='underline'>Media Consent Form (PDF)</li>
+                            <ul className='list-disc pl-6'>
+                                <li>Submit in person to Team Co-Captain Rohan Gawde.</li>
+                            </ul>
+                            <li>Have questions? Reach out to us at sanramonvalleyrobotics@gmail.com</li>
+                        </ul>
+                    </div>
+                    <div className='p-2'></div>
+                    <div className='px-10 text-[#fcc307] text-xl font-bold'>Requirements for Membership (2025/2026)</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>A. Join Google Classroom</div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>B. Submit the Membership Application Form</div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>C. Join the Team Remind</div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>D. Submit Liability Waiver (in person to Rohan G.)</div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>E. Submit Media Release Form (in person to Rohan G.)</div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>F. Complete Safety Training</div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>G. Complete One Subteam Training (may be waived at captains' discretion)</div>
                 </div>
-                <div className='p-2'></div>
-                <div className='px-10 text-black text-xl font-bold'>Requirements for Membership (2025/2026)</div>
-                <div className='p-2'></div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>A. Join Google Classroom</div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>B. Submit the Membership Application Form</div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>C. Join the Team Remind</div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>D. Submit Liability Waiver (in person to Rohan G.)</div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>E. Submit Media Release Form (in person to Rohan G.)</div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>F. Complete Safety Training</div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>G. Complete One Subteam Training (may be waived at captains' discretion)</div>
             </div>
         </section>
         <section>
-            <div className='text-left bg-white relative min-[110vh] w-screen'>
+            <div className='text-left bg-white relative h-[122vh] w-screen pl-12 pt-12'>
                 <img src='/join4.png' className='absolute h-[30vh] top-[10%] left-[80%] -translate-x-1/2' alt="Join 4"></img>
                 <img src='/join5.png' className='absolute h-[30vh] top-[40%] left-[80%] -translate-x-1/2' alt="Join 5"></img>
                 <img src='/join6.png' className='absolute h-[30vh] top-[70%] left-[80%] -translate-x-1/2' alt="Join 6"></img>
-                <div className='px-10 text-black text-2xl font-bold'>For Mentors:</div>
-                <div className='p-2'></div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>Our mentors are essential to the learning and success of our students. We welcome professionals, hobbyists, educators, and parents who want to contribute their time, knowledge, or industry experience to help shape the next generation of STEM leaders.</div>
-                <div className='p-2'></div>
-                <div className='px-10 text-black text-xl font-bold'>Who We’re Looking For</div>
-                <div className='p-2'></div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>
-                    <ul className='list-disc pl-6'>
-                        <li>Engineers (mechanical, electrical, software)</li>
-                        <li>Designers (CAD, prototyping)</li>
-                        <li>Programmers (Java, Python, embedded systems)</li>
-                        <li>Business professionals (fundraising, finance, organization)</li>
-                        <li>Educators or community leaders passionate about STEM</li>
-                        <li>Alumni or parents with time and interest</li>
-                    </ul>
+                <div className='bg-[#717070] w-15/24 py-4 h-[112vh]'>
+                    <div className='px-10 text-[#fcc307] text-2xl font-bold'>For Mentors:</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>Our mentors are essential to the learning and success of our students. We welcome professionals, hobbyists, educators, and parents who want to contribute their time, knowledge, or industry experience to help shape the next generation of STEM leaders.</div>
+                    <div className='p-2'></div>
+                    <div className='px-10 text-[#fcc307] text-xl font-bold'>Who We’re Looking For</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>
+                        <ul className='list-disc pl-6'>
+                            <li>Engineers (mechanical, electrical, software)</li>
+                            <li>Designers (CAD, prototyping)</li>
+                            <li>Programmers (Java, Python, embedded systems)</li>
+                            <li>Business professionals (fundraising, finance, organization)</li>
+                            <li>Educators or community leaders passionate about STEM</li>
+                            <li>Alumni or parents with time and interest</li>
+                        </ul>
+                    </div>
+                    <div className='p-2'></div>
+                    <div className='px-10 text-[#fcc307] text-xl font-bold'>What Mentors Do</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>
+                        <ul className='list-disc pl-6'>
+                            <li>Provide technical guidance and real-world insight</li>
+                            <li>Help students problem-solve and build critical thinking skills</li>
+                            <li>Support subteams during meetings and competitions</li>
+                            <li>Ensure safety and quality in shop and lab environments</li>
+                            <li>Serve as role models for teamwork, perseverance, and leadership</li>
+                        </ul>
+                    </div>
+                    <div className='p-2'></div>
+                    <div className='px-10 text-[#fcc307] text-xl font-bold'>Time Commitment</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>Flexible, based on your availability. Some mentors come weekly, while others assist during build season, events, or competitions.</div>
+                    <div className='p-2'></div>
+                    <div className='px-10 text-[#fcc307] text-xl font-bold'>Interested?</div>
+                    <div className='p-2'></div>
+                    <div className='text-left px-10 text-white text-lg max-w-3xl'>Contact us at sanramonvalleyrobotics@gmail.com</div>
+                    <div className='p-10'></div>
                 </div>
-                <div className='p-2'></div>
-                <div className='px-10 text-black text-xl font-bold'>What Mentors Do</div>
-                <div className='p-2'></div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>
-                    <ul className='list-disc pl-6'>
-                        <li>Provide technical guidance and real-world insight</li>
-                        <li>Help students problem-solve and build critical thinking skills</li>
-                        <li>Support subteams during meetings and competitions</li>
-                        <li>Ensure safety and quality in shop and lab environments</li>
-                        <li>Serve as role models for teamwork, perseverance, and leadership</li>
-                    </ul>
-                </div>
-                <div className='p-2'></div>
-                <div className='px-10 text-black text-xl font-bold'>Time Commitment</div>
-                <div className='p-2'></div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>Flexible, based on your availability. Some mentors come weekly, while others assist during build season, events, or competitions.</div>
-                <div className='p-2'></div>
-                <div className='px-10 text-black text-xl font-bold'>Interested?</div>
-                <div className='p-2'></div>
-                <div className='text-left px-10 text-black text-lg max-w-3xl'>Contact us at sanramonvalleyrobotics@gmail.com</div>
-                <div className='p-6'></div>
             </div>
         </section>
     </>
