ec6d82fb1a8591c566e0458c8be99aabde34ccbd
Nandini Ghata <152566146+nano13579@users.noreply.github.com>
2025-08-13 21:51:28 -0700

    a ***finally*** sane looking footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -2,35 +2,39 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div className="bg-[#1f1e23] overflow-auto max-h-[1/8vh]">
-            <div className='text-[#fcc307] text-lg py-4 px-6 text-left'>Team 1280 - The Ragin' C Biscuits</div>
-            <div className='flex flex-wrap justify-between text-white items-start px-6 text-center pb-4'>
-                <div className='grid gap-1'>
-                    <div className=''>Join Us</div>
-                    <div className=''>Resources</div>
-                    <div className=''>Sponsors</div>
-                    <div className=''>Sponsor Us</div>
-                </div>
-                <div className='grid gap-1'>
-                    <div className=''>About</div>
-                    <div className='flex gap-2'>
-                        <div className='grid gap-1'>
-                            <div className=''>The Team</div>
-                            <div className=''>Leaders</div>
-                            <div className=''>Facility</div>
+        <nav className="bg-[#1f1e23] text-white py-4">
+            <div className='container mx-auto flex flex-wrap md:items-center items-start'>
+                <div className='w-full md:w-3/5'>
+                    <div className='text-lg mb-4 text-[#fcc307]'>Team 1280 - The Ragin' C Biscuits</div>
+                    <div className='flex flex-col md:flex-row md:space-x-12 text-center'>
+                        <div className='mb-6 md:mb-0'>
+                            <div className='block mb-1'>Join Us</div>
+                            <div className='block mb-1'>Resources</div>
+                            <div className='block mb-1'>Sponsors</div>
+                            <div className='block mb-1'>Sponsor Us</div>
                         </div>
-                        <div className='grid gap-1'>
-                            <div className=''>FIRST</div>
-                            <div className=''>Mentors</div>
+                        <div className='block mb-2'>
                             <div className=''>About</div>
+                            <div className='flex space-x-8'>
+                                <div className=''>
+                                    <div className='block mb-1'>The Team</div>
+                                    <div className='block mb-1'>Leaders</div>
+                                    <div className='block mb-1'>Facility</div>
+                                </div>
+                                <div className='inline-block'>
+                                    <div className='block mb-1'>FIRST</div>
+                                    <div className='block mb-1'>Mentors</div>
+                                    <div className='block mb-1'>About</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='grid gap-1 items-start'>
-                    <img src='/footer2.png' className='h-3/4 object-contain pl-24'></img>
+                <div className='w-full flex items-center justify-center mt-6 md:w-2/5 space-x-4 md:mt-0'>
+                    <img src='/footer2.png' className='object-contain'></img>
                 </div>
             </div>
-        </div>
+        </nav>
     )
 }
 
