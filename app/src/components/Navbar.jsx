f854e3825c28297d856182329e2a7e07cd5c827a
Briar Chang <briartchang@gmail.com>
2025-08-15 01:18:51 -0700

    update home page font/title image

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <nav className="bg-[#1f1e23] text-white flex items-center px-6 py-4 justify-between">
             <div className="flex items-center w-1/8">  
                 <Link to='/' className='nav-item'> <img src='/logo.png' className="z-10"></img> </Link>
-                <Link to='/' className='nav-item'> <span className="text-[#fcc307] pl-[100px] font-bold text-3xl text-center whitespace-nowrap">Ragin C' Biscuits</span> </Link>
+                <Link to='/' className='nav-item'> <span className="text-[#fcc307] pl-[100px] font-orbitron text-3xl text-center whitespace-nowrap">Ragin C' Biscuits</span> </Link>
             </div>  
             <ul className='text-white font-bold'>
                 <Link to='/' className='nav-item'>Home</Link>
--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import TeamImage from '/team.png'
+import TeamImage from '/home4.png'
 import { Link } from 'react-router-dom';
 
 const Home = () => (
     <> 
         <section className="relative min-h-screen w-screen">
-            <div className="absolute inset-0 bg-[url('/team.png')] bg-cover bg-center opacity-70 z-0"></div>
+            <div className="absolute inset-0 bg-[url('/home4.png')] bg-cover bg-center opacity-80 z-0"></div>
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="relative z-10 text-white flex items-center min-w-full justify-center min-h-screen">
-                <h1 className="absolute top-[30%] left-1/2 min-w-full text-6xl font-bold text-white transform -translate-x-1/2 text-center">FRC Team 1280 - Ragin' C Biscuits</h1>
-                <p className="absolute top-[50%] left-1/2 text-3xl text-white font-bold transform -translate-x-1/2 text-center">San Ramon Valley Robotics</p>
+                <h1 className="absolute top-[30%] left-1/2 min-w-full text-6xl font-orbitron text-yellow-500 transform -translate-x-1/2 text-center">FRC Team 1280 - Ragin' C Biscuits</h1>
+                <p className="absolute top-[50%] left-1/2 text-3xl text-white font-orbitron transform -translate-x-1/2 text-center">San Ramon Valley Robotics</p>
             </div>
         </section>
         <section>
