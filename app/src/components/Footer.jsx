e32ed6d70d96bc9edaa4e7b45766946f0b661770
Briar Chang <briartchang@gmail.com>
2025-08-15 01:48:03 -0700

    footer links

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,30 +1,31 @@
 import './Footer.css'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <nav className="bg-[#1f1e23] text-white px-4 py-4">
             <div className='container mx-auto flex flex-wrap md:items-center items-start'>
                 <div className='w-full md:w-3/5'>
-                    <div className='text-lg mb-4 text-[#fcc307]'>Team 1280 - The Ragin' C Biscuits</div>
+                    <Link to='/' className='nav-item'> <div className='text-lg mb-4 text-[#fcc307]'>Team 1280 - The Ragin' C Biscuits</div> </Link>
                     <div className='flex flex-col md:flex-row md:space-x-12 text-center'>
                         <div className='mb-6 md:mb-0'>
-                            <div className='block mb-1'>Join Us</div>
-                            <div className='block mb-1'>Resources</div>
-                            <div className='block mb-1'>Sponsors</div>
-                            <div className='block mb-1'>Sponsor Us</div>
+                            <Link to='/Join' className='nav-item'> <div className='block mb-1'>Join Us</div> </Link>
+                            <Link to='/Resources' className='nav-item'> <div className='block mb-1'>Resources</div> </Link>
+                        </div>
+                        <div className='mb-6 md:mb-0'>
+                            <Link to='/SponsorsDonors' className='nav-item'> <div className='block mb-1'>Sponsors</div> </Link>
+                            <Link to='/Donate' className='nav-item'> <div className='block mb-1'>Sponsor Us</div> </Link>
                         </div>
                         <div className='block mb-2'>
-                            <div className=''>About</div>
+                            <Link to='/About' className='nav-item'> <div className=''>About</div> </Link>
                             <div className='flex space-x-8'>
                                 <div className=''>
-                                    <div className='block mb-1'>The Team</div>
-                                    <div className='block mb-1'>Leaders</div>
-                                    <div className='block mb-1'>Facility</div>
+                                    <Link to='/Leadership' className='nav-item'> <div className='block mb-1'>Leaders</div> </Link>
+                                    <Link to='/Facility' className='nav-item'> <div className='block mb-1'>Facility</div> </Link>
                                 </div>
                                 <div className='inline-block'>
-                                    <div className='block mb-1'>FIRST</div>
-                                    <div className='block mb-1'>Mentors</div>
-                                    <div className='block mb-1'>About</div>
+                                    <Link to='/First' className='nav-item'>  <div className='block mb-1'>FIRST</div> </Link>
+                                    <Link to='/Mentors' className='nav-item'> <div className='block mb-1'>Mentors</div> </Link>
                                 </div>
                             </div>
                         </div>
