b07fb05bbe1833d77c4a9c6ffeb76d070c523792
Briar Chang <briartchang@gmail.com>
2025-08-14 20:08:05 -0700

    link to FIRST website

--- app/src/pages/First.jsx
+++ app/src/pages/First.jsx
@@ -9,15 +9,17 @@ const First = () => {
             <section>
                 <div className='text-left bg-white relative h-[95vh] w-screen pl-12'>
                     <div className='px-10 pt-12 text-black text-5xl font-bold'>FIRST®</div>
-                    <hr className="my-8 border-gray-300 border-t-4 w-17/18"></hr>
+                    <hr className="mx-10 my-8 border-gray-300 border-t-4 w-17/18"></hr>
                     <div className="text-lg max-w-[90vw]">
-                        <div className="text-gray-500">FIRST® (For Inspiration and Recognition of Science and Technology) is a non-profit, international organization founded in 1989 by inventor and entrepreneur Dean Kamen. Its mission is to inspire and educate young people in the fields of science, technology, engineering, and math (STEM) through a mentor-based teaching system.</div>
+                        <div className="px-10 text-gray-500">FIRST® (For Inspiration and Recognition of Science and Technology) is a non-profit, international organization founded in 1989 by inventor and entrepreneur Dean Kamen. Its mission is to inspire and educate young people in the fields of science, technology, engineering, and math (STEM) through a mentor-based teaching system.</div>
                         <div className="p-6"></div>
-                        <div className="text-gray-500">FIRST® runs several programs for different age groups, including the Junior LEGO® League, FIRST® LEGO® League (FLL), FIRST® Tech Challenge (FTC), and the flagship FIRST® Robotics Competition (FRC), which Ragin’ C Biscuits competes in. Each year, FIRST® releases a new game for each program, challenging teams to strategize, design, prototype, build, program, and test their robot within a six-week build season. Afterward, teams compete in regional events for a chance to qualify for the annual World Championship in Houston, Texas.</div>
+                        <div className="px-10 text-gray-500">FIRST® runs several programs for different age groups, including the Junior LEGO® League, FIRST® LEGO® League (FLL), FIRST® Tech Challenge (FTC), and the flagship FIRST® Robotics Competition (FRC), which Ragin’ C Biscuits competes in. Each year, FIRST® releases a new game for each program, challenging teams to strategize, design, prototype, build, program, and test their robot within a six-week build season. Afterward, teams compete in regional events for a chance to qualify for the annual World Championship in Houston, Texas.</div>
                         <div className="p-6"></div>
-                        <div className="text-gray-500">In the world of FIRST®, two core values stand out: Gracious Professionalism® and Coopertition®. Gracious Professionalism, a term coined by FIRST® advisor Woodie Flowers, encourages teams to compete honorably while respecting one another. Coopertition builds on this idea, promoting collaboration and mutual learning even during competition.</div>
+                        <div className="px-10 text-gray-500">In the world of FIRST®, two core values stand out: Gracious Professionalism® and Coopertition®. Gracious Professionalism, a term coined by FIRST® advisor Woodie Flowers, encourages teams to compete honorably while respecting one another. Coopertition builds on this idea, promoting collaboration and mutual learning even during competition.</div>
                         <div className="p-6"></div>
-                        <div className="text-gray-500">For more about FIRST®, visit: https://www.firstinspires.org</div>
+                        <div className="px-10 text-gray-500">For more about FIRST®, visit 
+                            <a href="https://www.firstinspires.org" className="font-medium !text-yellow-500 dark:!text-yellow-500 hover:underline"> the FIRST® website! </a>
+                         </div>
                     </div>
                 </div>
             </section>
