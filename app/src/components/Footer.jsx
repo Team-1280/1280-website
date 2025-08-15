20aaf1bda1163b54d1d1224e02e49b39d95045ac
aatle <168398276+aatle@users.noreply.github.com>
2025-08-14 21:39:55 -0700

    Alignment changes

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -1,19 +1,19 @@
-import './Footer.css'
+import './Footer.css';
 
 const Footer = () => {
     return (
         <nav className="bg-[#1f1e23] text-white px-4 py-4">
             <div className='container mx-auto flex flex-wrap md:items-center items-start'>
                 <div className='w-full md:w-3/5'>
-                    <div className='text-lg mb-4 text-[#fcc307]'>Team 1280 - The Ragin' C Biscuits</div>
-                    <div className='flex flex-col md:flex-row md:space-x-12 text-center'>
-                        <div className='mb-6 md:mb-0'>
+                    <div className='text-lg mb-4 text-[#fcc307] font-bold'>Team 1280 - The Ragin' C Biscuits</div>
+                    <div className='flex flex-col md:flex-row md:space-x-12'>
+                        <div className='mb-6 md:mb-0 text-left'>
                             <div className='block mb-1'>Join Us</div>
                             <div className='block mb-1'>Resources</div>
                             <div className='block mb-1'>Sponsors</div>
                             <div className='block mb-1'>Sponsor Us</div>
                         </div>
-                        <div className='block mb-2'>
+                        <div className='block mb-2 text-center'>
                             <div className=''>About</div>
                             <div className='flex space-x-8'>
                                 <div className=''>
