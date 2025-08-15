8b62955ce7cd6ec69bd71d497e584f04744a814d
aatle <168398276+aatle@users.noreply.github.com>
2025-08-14 21:06:36 -0700

    Fix navbar dropdown hover issue

--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
                 <img src='./logo.png' className="w-1/2 h-auto"/>
                 <span className="text-[#fcc307] pl-[100px] font-bold text-3xl text-center whitespace-nowrap">Ragin C' Biscuits</span>
             </div>  
-            <ul className='text-white font-bold'>
+            <ul className='text-white font-bold z-1000'>
                 <Link to='/' className='nav-item'>Home</Link>
                 <div className="dropdown">
                     <Link to='/About' className='drop-link'>About</Link>
