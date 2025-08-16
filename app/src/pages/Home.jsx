1a360dd9919a27f0fc3b40944711e30c0e248dd3
aatle <168398276+aatle@users.noreply.github.com>
2025-08-16 16:50:08 -0700

    Draft gray box style example

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => (
             <div className="relative min-h-screen w-full">
                 <div className="absolute inset-0 bg-white flex px-4 pt-12 pb-12">
                     <img src="/home3.png" className='pr-8'></img>
-                    <div className='absolute top-[7%] bg-[#717070] left-[22%] justify-center grid items-center w-6/8 h-1/4'>
+                    <div className='absolute top-[7%] bg-linear-to-t from-[#717070] to-[#888888] rounded-lg border border-[#aaaaaa] border-1 shadow-lg/20 left-[22%] justify-center grid items-center w-6/8 h-1/4'>
                         <div className='text-[#fcc307] font-bold text-3xl pt-6 px-4 pb-2'>Team 1280 - Who We Are</div>
                         <div className='text-white text-lg px-4 pb-60'>FRC Team 1280, the Ragin' C’ Biscuits, is a student-led robotics team based at San Ramon Valley High School in Danville, California. Since 2004, we’ve empowered high school students to explore STEM through hands-on engineering, programming, business, and leadership challenges.</div>
                     </div>
