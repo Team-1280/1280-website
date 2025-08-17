c170eae9d72d72429e2ad1c9b6ff14a77bf97de1
Briar Chang <briartchang@gmail.com>
2025-08-17 10:30:36 -0700

    reformat footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -5,27 +5,36 @@ const Footer = () => {
     return (
         <nav className="bg-[#1f1e23] text-white h-48">
             <div className='container max-w-full h-full flex flex-nowrap items-start'>
+                <div class="absolute ml-75 mt-5 h-40 w-px bg-[#fcc307]"></div>
                 <div className='w-full h-full px-8 py-5 flex flex-col items-start font-bricolage'>
+                    <div className="flex flex-row gap-60">
                     <Link to='/' className='nav-item'>
-                        <div className='text-lg mb-4 text-[#fcc307] font-bold w-auto whitespace-nowrap'>Team 1280 - The Ragin' C Biscuits</div>
+                        <div className='text-lg mb-5 text-[#fcc307] font-bold w-auto whitespace-nowrap'>Team 1280</div>
                     </Link>
-                    <div className='flex flex-col flex-row gap-12 text-center flex-nowrap'>
+                    <div className='text-lg mb-5 text-[#fcc307] font-bold w-auto whitespace-nowrap'>About</div>
+                    </div>
+                    <div className='flex flex-col flex-row gap-12 text-left flex-nowrap'>
                         <div className='text-left'>
-                            <Link to='/Join' className='nav-item'><div className='block mb-1'>Join Us</div></Link>
+                            <Link to='/Join' className='nav-item'><div className='block mb-5'>Join Us</div></Link>
                             <Link to='/Contact' className='nav-item'><div className='block mb-1'>Resources</div></Link>
-                            <Link to='/SponsorsDonors' className='nav-item'><div className='block mb-1'>Sponsors</div></Link>
+                        </div>
+                        <div>
+                            <Link to='/SponsorsDonors' className='nav-item'><div className='block mb-5'>Sponsors</div></Link>
                             <Link to='/Donate' className='nav-item'><div className='block mb-1'>Sponsor Us</div></Link>
                         </div>
-                        <div className='text-center'>
-                            <Link to='/About' className='nav-item'> <div className='mb-2'>About</div> </Link>
-                            <div className='flex gap-8 flex-nowrap'>
+                        <div className='text-left'>
+                            <div className='flex gap-8 flex-nowrap ml-17'>
                                 <div className=''>
-                                    <Link to='/Leadership' className='nav-item'> <div className='block mb-1'>Leaders</div> </Link>
-                                    <Link to='/Facility' className='nav-item'> <div className='block mb-1'>Facility</div> </Link>
+                                    <Link to='/About' className='nav-item'> <div className='block mb-5'>The Team</div> </Link>
+                                    <Link to='/First' className='nav-item'>  <div className='block mb-1'>FIRST</div> </Link>
                                 </div>
                                 <div className='inline-block'>
-                                    <Link to='/First' className='nav-item'>  <div className='block mb-1'>FIRST</div> </Link>
+                                    <Link to='/Leadership' className='nav-item'> <div className='block mb-5'>Leaders</div> </Link>
                                     <Link to='/Mentors' className='nav-item'> <div className='block mb-1'>Mentors</div> </Link>
+                                </div>
+                                <div className='inline-block'>
+                                    <Link to='/Facility' className='nav-item'> <div className='block mb-5'>Facility</div> </Link>
+                                    <Link to='/Outreach' className='nav-item'> <div className='block mb-1'>Outreach</div> </Link>
                                 </div>
                             </div>
                         </div>
