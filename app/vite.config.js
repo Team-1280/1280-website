4bbc868ce470282bccf59bb50bc96839a7970eed
aatle <168398276+aatle@users.noreply.github.com>
2025-09-20 12:20:47 -0700

    Add sitemap generator

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,8 +1,9 @@
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), Sitemap({ hostname: 'https://www.team1280.com' })],
 })
