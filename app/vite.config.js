da789fce6ae6b8851a0315c4a35eb3ed5a58446a
aatle <168398276+aatle@users.noreply.github.com>
2025-09-20 13:39:04 -0700

    Update sitemap

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,5 +5,29 @@ import Sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), Sitemap({ hostname: 'https://www.team1280.com' })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Sitemap({
+      hostname: 'https://www.team1280.com',
+      changefreq: 'weekly',
+      dynamicRoutes: [
+        '/',
+        '/calendar',
+        '/about',
+        '/outreach',
+        '/resources',
+        '/contact',
+        '/join',
+        '/leadership',
+        '/news',
+        '/robot-engineering',
+        '/sponsors',
+        '/donate',
+        '/mentors',
+        '/facility',
+        '/first',
+      ],
+    }),
+  ],
 })
