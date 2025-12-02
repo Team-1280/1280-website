42448b0b8cfb3771bc8a154758b02679f9ece921
aatle <168398276+aatle@users.noreply.github.com>
2025-12-01 19:22:49 -0800

    Attempt to fix favicon

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -4,6 +4,11 @@ const Home = () => (
   <>
     <head>
       <link rel="canonical" href="https://www.team1280.com" />
+      <link rel="icon" type="image/png" href="/favicon-96x94.png" sizes="96x96" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="manifest" href="/site.webmanifest" />
       <title>Team 1280</title>
     </head>
     <section className="relative h-[50dvw] w-full">
